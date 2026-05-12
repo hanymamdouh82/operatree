0852eefd2060922372d1feea0d288ec88d289f45
Hany Mamdouh <hany@atentec.com>
2026-05-12 21:06:05 +0300

    Feat: initial commit

---
+++ cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome OperaTree...A project operating system built on your filesystem...!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hanymamdouh82/operatree
+
+go 1.22.0
